933bfa66ac32121001074e6a102580c3c332f2fa
Tunde <babatea4real@yahoo.com>
2023-09-21 14:18:57 +0200

    react routing intro

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// App.jsx
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ErrorPage from "./pages/ErrorPage";
+
+import { Routes, Route } from "react-router-dom";
+import HomePageWithNavigate from "./pages/HomePageWithNavigate";
+import projectsData from "./projects-data.json";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePageWithNavigate />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/projects"
+          element={<ProjectsPage projects={projectsData} />}
+        />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
